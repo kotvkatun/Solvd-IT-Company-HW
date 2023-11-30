a3e63efd9207951f276814731cecf983195e90f6
tkarsakov <115474596+tkarsakov@users.noreply.github.com>
2023-11-30 15:50:10 +0300

    Refactor ITCompany to initialize developerList with a block

--- src/main/java/com/solvd/classes/itcompany/ITCompany.java
+++ src/main/java/com/solvd/classes/itcompany/ITCompany.java
@@ -2,25 +2,33 @@ package com.solvd.classes.itcompany;
 
 import com.solvd.classes.developer.AbstractDeveloper;
 import com.solvd.classes.developer.Developer;
-import com.solvd.classes.initializer.DeveloperInitializer;
+import com.solvd.classes.developer.Grade;
 import com.solvd.classes.interfaces.Addable;
 import com.solvd.classes.interfaces.Clearable;
 import com.solvd.classes.project.Project;
 import com.solvd.classes.project.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class ITCompany implements Clearable, Addable {
     private String itCompanyName;
     private Project project;
-    private List<Developer> developerList = ITCompany.initializeDeveloperList();
+    private List<Developer> developerList;
+
+    {
+        Developer junVasya = new Developer(Grade.JUNIOR, "Vasya Pupkin");
+        Developer midPetya = new Developer(Grade.MIDDLE, "Petya Petrov");
+        Developer senPomidor = new Developer(Grade.SENIOR, "Senior Pomidor");
+        List<Developer> developerList = new ArrayList<>();
+        developerList.add(junVasya);
+        developerList.add(midPetya);
+        developerList.add(senPomidor);
+        this.developerList = developerList;
+    }
 
     public ITCompany(String itCompanyName) {
         this.itCompanyName = itCompanyName;
-    }
-
-    private static List<Developer> initializeDeveloperList() {
-        return DeveloperInitializer.initializeDeveloperList();
     }
 
     public String getItCompanyName() {
