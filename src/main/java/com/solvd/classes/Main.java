1b7a723ce10ee5489215f42c7e8a10b83d6eb47e
tkarsakov <115474596+tkarsakov@users.noreply.github.com>
2023-12-06 14:58:45 +0300

    Replace every sysout call with a logging operation

--- src/main/java/com/solvd/classes/Main.java
+++ src/main/java/com/solvd/classes/Main.java
@@ -1,69 +1,71 @@
 package com.solvd.classes;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.System.out;
-
 public class Main {
+    private final static Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         //MenuSwitch menu = new MenuSwitch();
         //menu.mainMenuSwitch();
         Class<Reflect> reflectClass = Reflect.class;
-        out.format("Class:%n  %s%n%n", reflectClass.getCanonicalName());
-        out.format("Modifiers:%n  %s%n%n",
+        LOGGER.info("Class:\n  {}\n\n", reflectClass.getCanonicalName());
+        LOGGER.info("Modifiers:\n  {}\n\n",
                 Modifier.toString(reflectClass.getModifiers()));
         printMembers(reflectClass.getDeclaredConstructors(), "Constructor");
         printMembers(reflectClass.getDeclaredFields(), "Fields");
         printMembers(reflectClass.getMethods(), "Methods");
         printClasses(reflectClass);
 
-        out.format("Type Parameters:%n");
+        LOGGER.info("Type Parameters:\n");
         TypeVariable[] tv = reflectClass.getTypeParameters();
         if (tv.length != 0) {
-            out.format("  ");
+            LOGGER.info("  ");
             for (TypeVariable t : tv)
-                out.format("%s ", t.getName());
-            out.format("%n%n");
+                LOGGER.info("{} ", t.getName());
+            LOGGER.info("\n\n");
         } else {
-            out.format("  -- No Type Parameters --%n%n");
+            LOGGER.info("  -- No Type Parameters --\n\n");
         }
 
-        out.format("Implemented Interfaces:%n");
+        LOGGER.info("Implemented Interfaces:\n");
         Type[] intfs = reflectClass.getGenericInterfaces();
         if (intfs.length != 0) {
             for (Type intf : intfs)
-                out.format("  %s%n", intf.toString());
-            out.format("%n");
+                LOGGER.info("  {}\n", intf.toString());
+            LOGGER.info("\n");
         } else {
-            out.format("  -- No Implemented Interfaces --%n%n");
+            LOGGER.info("  -- No Implemented Interfaces --\n\n");
         }
 
-        out.format("Inheritance Path:%n");
+        LOGGER.info("Inheritance Path:\n");
         List<Class> l = new ArrayList<Class>();
         printAncestor(reflectClass, l);
         if (l.size() != 0) {
             for (Class<?> cl : l)
-                out.format("  %s%n", cl.getCanonicalName());
-            out.format("%n");
+                LOGGER.info("  {}\n", cl.getCanonicalName());
+            LOGGER.info("\n");
         } else {
-            out.format("  -- No Super Classes --%n%n");
+            LOGGER.info("  -- No Super Classes --\n\n");
         }
 
-        out.format("Annotations:%n");
+        LOGGER.info("Annotations:\n");
         Annotation[] ann = reflectClass.getAnnotations();
         if (ann.length != 0) {
             for (Annotation a : ann)
-                out.format("  %s%n", a.toString());
-            out.format("%n");
+                LOGGER.info("  {}\n", a.toString());
+            LOGGER.info("\n");
         } else {
-            out.format("  -- No Annotations --%n%n");
+            LOGGER.info("  -- No Annotations --\n\n");
         }
         try {
             Constructor<Reflect> reflectConstructor = reflectClass.getConstructor(String.class, int.class);
-            out.println(reflectConstructor.newInstance("Hello", 1).toString());
+            LOGGER.info(reflectConstructor.newInstance("Hello", 1).toString());
         } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
                  InvocationTargetException e) {
             throw new RuntimeException(e);
@@ -71,28 +73,28 @@ public class Main {
     }
 
     private static void printMembers(Member[] mbrs, String s) {
-        out.format("%s:%n", s);
+        LOGGER.info("{}:\n", s);
         for (Member mbr : mbrs) {
             if (mbr instanceof Field)
-                out.format("  %s%n", ((Field) mbr).toGenericString());
+                LOGGER.info("  {}\n", ((Field) mbr).toGenericString());
             else if (mbr instanceof Constructor)
-                out.format("  %s%n", ((Constructor<?>) mbr).toGenericString());
+                LOGGER.info("  {}\n", ((Constructor<?>) mbr).toGenericString());
             else if (mbr instanceof Method)
-                out.format("  %s%n", ((Method) mbr).toGenericString());
+                LOGGER.info("  {}\n", ((Method) mbr).toGenericString());
         }
         if (mbrs.length == 0)
-            out.format("  -- No %s --%n", s);
-        out.format("%n");
+            LOGGER.info("  -- No {} --\n", s);
+        LOGGER.info("\n");
     }
 
     private static void printClasses(Class<?> c) {
-        out.format("Classes:%n");
+        LOGGER.info("Classes:\n");
         Class<?>[] clss = c.getClasses();
         for (Class<?> cls : clss)
-            out.format("  %s%n", cls.getCanonicalName());
+            LOGGER.info("  {}\n", cls.getCanonicalName());
         if (clss.length == 0)
-            out.format("  -- No member interfaces, classes, or enums --%n");
-        out.format("%n");
+            LOGGER.info("  -- No member interfaces, classes, or enums --\n");
+        LOGGER.info("\n");
     }
 
     private static void printAncestor(Class<?> c, List<Class> l) {
