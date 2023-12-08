1b85e909fe277be0e58d31dfad90f1915456122a
tkarsakov <115474596+tkarsakov@users.noreply.github.com>
2023-12-08 14:09:13 +0300

    Turn threads into CompletableFuture as well

--- src/main/java/com/solvd/classes/Main.java
+++ src/main/java/com/solvd/classes/Main.java
@@ -16,24 +16,20 @@ public class Main {
         //menu.mainMenuSwitch();
         ConnectionPool connectionPool = ConnectionPool.INSTANCE;
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Thread thread1 = new Thread(ConnectionTask.getConnectionTask(1000, LOGGER, connectionPool));
-        Thread thread2 = new Thread(ConnectionTask.getConnectionTask(1000, LOGGER, connectionPool));
         List<CompletableFuture<Void>> connectionFutures = new ArrayList<>();
+        connectionFutures.add(CompletableFuture
+                .runAsync(ConnectionTask.getConnectionTask(1000, LOGGER, connectionPool)));
+        connectionFutures.add(CompletableFuture
+                .runAsync(ConnectionTask.getConnectionTask(1000, LOGGER, connectionPool)));
+
         for (int i = 0; i < 5; i++) {
             connectionFutures.add(CompletableFuture
                     .runAsync(ConnectionTask
                             .getConnectionTask(2000, LOGGER, connectionPool), executorService));
         }
-        try {
-            thread1.start();
-            thread2.start();
-            thread2.join();
-            thread1.join();
-            connectionFutures.stream().map(CompletableFuture::join);
-            executorService.shutdown();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+
+        connectionFutures.stream().map(CompletableFuture::join);
+        executorService.shutdown();
     }
 }
 
